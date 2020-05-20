faf0277590cd0624443043b00fc817cfd3428708
gastons92 <65659297+gastons92@users.noreply.github.com>
2020-05-20 19:55:07 -0300

    gaston
    app contiene el main
    panel rebote es un jpanel donde rebota la pelota
    en ventana se agrega el jpanel (donde rebota la pelota) y un panel de botones (uno para agregar pelotas y otro para apagar todos los hilos)
    -->hay que cambiar la pelota por una imagen
    -->hay que poder agregar varias imagenes...

---
+++ src/app/App.java
@@ -0,0 +1,11 @@
+package app;
+
+public class App {
+    public static void main(String[] args) {
+        try{
+            Ventana protector = new Ventana();
+            protector.setLocationRelativeTo(null);
+        }catch(InterruptedException e){}
+        
+    }
+}
